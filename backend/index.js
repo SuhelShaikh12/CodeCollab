61c7a336a45e203ebccdf7b064471334f24b6c0a
SuhelShaikh12 <157576628+SuhelShaikh12@users.noreply.github.com>
2025-10-08 00:38:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -75,7 +75,7 @@ const port =process.env.PORT || 3000;
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get((req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
